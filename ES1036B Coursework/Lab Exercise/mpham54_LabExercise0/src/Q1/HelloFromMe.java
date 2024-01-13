154af080c2e708ac2adabf655eeeec2e1824504d
Quang Pham <125075617+QMP0503@users.noreply.github.com>
2024-01-13 13:54:43 -0500

    Delete ES1036B Coursework directory
    wrong name

--- ES1036B Coursework/Lab Exercise/mpham54_LabExercise0/src/Q1/HelloFromMe.java
+++
@@ -1,7 +0,0 @@
-package Q1;
-public class HelloFromMe {
-    public static void main(String[] args) {
-        //System.out.print("Life is beautiful! ");
-        System.out.println("YES!");
-    }
-}
--- ES1036B Coursework/Lab Exercise/mpham54_LabExercise4/src/Q2/VolumeOf3DObjectsInterface.java
+++
@@ -1,5 +0,0 @@
-package Q2;
-
-public interface VolumeOf3DObjectsInterface {
-    public abstract double getVolume();
-}
